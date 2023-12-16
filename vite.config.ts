5e937b1d3a64326cea4fd733fc15122709059825
Zheng Hao <zhengh513@outlook.com>
2023-12-16 13:20:54 +0800

    chore: 安装和配置 Vant

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
@@ -31,6 +32,7 @@ export default defineConfig({
       },
     }),
     Components({
+      resolvers: [VantResolver()],
       dts: true,
       types: [
         {
