55e67a7bcbe294f2e408eb043b90aae5f8fc3977
Zheng Hao <zhengh513@outlook.com>
2023-12-17 21:42:27 +0800

    refactor(page-layout): 在二级页面中取消底部 padding

--- src/layouts/PageLayout.vue
+++ src/layouts/PageLayout.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="page-layout">
+  <div
+    class="page-layout"
+    :class="{ 'page-layout--secondary': isSecondaryPage }"
+  >
     <slot />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute()
+
+const isSecondaryPage = computed(() => {
+  return !['home', 'user'].includes(route.name as string)
+})
+</script>
 
 <style scoped>
 .page-layout {
@@ -12,5 +21,9 @@
   padding-bottom: 50px;
   margin-top: 0;
   min-height: 100vh;
+}
+
+.page-layout--secondary {
+  padding-bottom: 0;
 }
 </style>
