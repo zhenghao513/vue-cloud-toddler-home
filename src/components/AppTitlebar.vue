67296e298208e95fdb33438f08dd28bbc8e2d68c
Zheng Hao <zhengh513@outlook.com>
2023-12-18 14:59:15 +0800

    feat: 标题栏组件

---
+++ src/components/AppTitlebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    :style="{ backgroundColor: props.backgroundColor }"
+    class="title-bar"
+  >
+    {{ props.title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+  backgroundColor?: string
+}>()
+</script>
+
+<style scoped>
+.title-bar {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+}
+</style>
