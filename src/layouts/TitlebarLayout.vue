ded9f1cd10ef56f1bc166cb0f8af7720f429d505
Zheng Hao <zhengh513@outlook.com>
2023-12-25 15:12:24 +0800

    refactor: 将左右内边距添加到标签栏布局中

--- src/layouts/TitlebarLayout.vue
+++ src/layouts/TitlebarLayout.vue
@@ -1,10 +1,17 @@
 <template>
   <PageLayout>
     <AppTitlebar v-bind="$attrs" />
-    <slot></slot>
+    <div class="slot-container">
+      <slot></slot>
+    </div>
   </PageLayout>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.slot-container {
+  padding-left: var(--default-padding-start);
+  padding-right: var(--default-padding-end);
+}
+</style>
