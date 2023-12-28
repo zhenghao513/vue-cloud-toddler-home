7a81672ead6bdaf486b4da71c5bbb260398267c5
Zheng Hao <zhengh513@outlook.com>
2023-12-28 16:00:35 +0800

    feat: 新增五彩纸屑组件

---
+++ src/components/ConfettiButton.vue
@@ -0,0 +1,102 @@
+<template>
+  <div @click="$emit('click')">
+    <VanButton
+      v-bind="buttonProps"
+      @click="handleClick"
+    >
+      <slot></slot>
+    </VanButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ButtonProps } from 'vant'
+import type { ConfettiButtonProps } from '@/types/component'
+import canvasConfetti from 'canvas-confetti'
+import confetti from '@/utils/confetti'
+
+const props = withDefaults(defineProps<ConfettiButtonProps>(), {
+  particleCount: 60,
+  angle: 90,
+  spread: 70,
+  startVelocity: 25,
+  gravity: 1.2,
+  drift: 0,
+  ticks: 150,
+  colors: () => ['#F76560', '#FADC19', '#14C9C9', '#722ED1', '#FF9A2E'],
+  shapes: () => ['square'],
+  scalar: 1,
+
+  // Vant Button Props 默认值
+  type: 'default',
+  size: 'normal',
+  iconPrefix: 'van-icon',
+  iconPosition: 'left',
+  tag: 'button',
+  nativeType: 'button',
+  block: false,
+  plain: false,
+  square: false,
+  round: false,
+  disabled: false,
+  hairline: false,
+  loading: false,
+  loadingType: 'circular',
+  loadingSize: '20px',
+  replace: false,
+})
+
+defineEmits<{
+  (e: 'click'): void
+}>()
+
+const buttonProps = computed<ButtonProps>(() => {
+  return {
+    block: props.block,
+    disabled: props.disabled,
+    hairline: props.hairline,
+    iconPosition: props.iconPosition,
+    loading: props.loading,
+    nativeType: props.nativeType,
+    plain: props.plain,
+    replace: props.replace,
+    round: props.round,
+    size: props.size,
+    square: props.square,
+    tag: props.tag,
+    type: props.type,
+    color: props.color,
+    icon: props.icon,
+    iconPrefix: props.iconPrefix,
+    loadingSize: props.loadingSize,
+    loadingText: props.loadingText,
+    loadingType: props.loadingType,
+    text: props.text,
+    to: props.to,
+    url: props.url,
+  }
+})
+
+const confettiOptions = computed<canvasConfetti.Options>(() => {
+  return {
+    particleCount: props.particleCount,
+    angle: props.angle,
+    spread: props.spread,
+    startVelocity: props.startVelocity,
+    decay: props.decay,
+    gravity: props.gravity,
+    drift: props.drift,
+    ticks: props.ticks,
+    origin: props.origin,
+    colors: props.colors,
+    shapes: props.shapes,
+    scalar: props.scalar,
+  }
+})
+
+const handleClick = () => {
+  confetti(confettiOptions.value)
+}
+</script>
+
+<style scoped></style>
---
+++ src/types/component.ts
@@ -0,0 +1,74 @@
+import type { Origin, Shape } from 'canvas-confetti'
+import type { ButtonProps } from 'vant'
+
+export interface ConfettiButtonProps extends ButtonProps {
+  /**
+   * 要发射的五彩纸屑数量。
+   * @default 60
+   */
+  particleCount?: number
+  /**
+   * 发射五彩纸屑的角度，以度为单位。
+   * 90 是直线上升。
+   * @default 90
+   */
+  angle?: number
+  /**
+   * 五彩纸屑可以偏离中心多远，以度为单位。
+   * 45 表示五彩纸屑将以定义的正负 22.5 度角发射。
+   */
+  spread?: number
+  /**
+   * 五彩纸屑开始移动的速度，以像素为单位。
+   * @default 25
+   */
+  startVelocity?: number
+  /**
+   * 五彩纸屑会以多快的速度消失。
+   * 将此数字保持在 0 和 1 之间，否则五彩纸屑会加速消失。
+   * 如果你不确定的话，尽量不要改变它。
+   */
+  decay?: number
+  /**
+   * 五彩纸屑被拉下的速度有多快。
+   * 1 是全重力，0.5 是半重力等。 如果你愿意，你甚至可以使粒子上升。
+   * @default 1.2
+   */
+  gravity?: number
+  /**
+   * 五彩纸屑会向一侧漂移多少。
+   * 默认值为 0，这意味着它们会直线下降。
+   * 左边使用负数，右边使用正数。
+   * @default 0
+   */
+  drift?: number
+  /**
+   * 五彩纸屑会移动多少次。
+   * @default 150
+   */
+  ticks?: number
+  /**
+   * 从哪里开始发射纸屑。 如果您愿意，可以随意在屏幕外启动。
+   * origin.x：页面上的 x 位置，0 为左边缘，1 为右边缘。
+   * origin.y：页面上的 y 位置，0 为顶边，1 为底边。
+   */
+  origin?: Origin
+  /**
+   * 十六进制格式的颜色字符串数组。
+   * @default ['#F76560', '#FADC19', '#14C9C9', '#722ED1', '#FF9A2E']
+   */
+  colors?: string[]
+  /**
+   * 五彩纸屑的一系列形状。
+   * 可能的值为正方形和圆形。
+   * 默认是在均匀混合中使用两种形状。
+   * 你甚至可以通过提供诸如 ['circle', 'square'] 之类的值来更改组合，以使用三分之二的圆和三分之一的正方形。
+   * @default ["square"]
+   */
+  shapes?: Shape[]
+  /**
+   * 每个五彩纸屑粒子的比例缩放。
+   * @default 1
+   */
+  scalar?: number
+}
