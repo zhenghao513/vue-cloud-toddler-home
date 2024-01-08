6868293d45cad06048631de8dd3b6cd19815c122
Zheng Hao <zhengh513@outlook.com>
2024-01-08 19:46:01 +0800

    refactor: 忽略未使用的参数的编译错误

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,6 +23,8 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+
+  // @ts-ignore: It's ok to ignore this compile error
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition
