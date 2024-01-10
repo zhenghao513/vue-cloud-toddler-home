89ff2509dc738b94f717c9fb70e69844e0ce61d8
Zheng Hao <zhengh513@outlook.com>
2024-01-10 16:25:51 +0800

    refactor: 将特色课程页面列表更改为虚拟列表

--- src/views/CharacteristicCourse.vue
+++ src/views/CharacteristicCourse.vue
@@ -3,19 +3,45 @@
     title="特色课程"
     background-color="#F7CE00"
   >
-    <VanCard
-      v-for="course in data?.courses"
-      :key="course.id"
-      :num="course.duration"
-      :price="formatPrice(course.price)"
-      :desc="course.description"
-      :title="course.name"
-      thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg"
-    />
+    <Grid
+      :length="length"
+      :pageSize="10"
+      :pageProvider="pageProvider"
+      class="grid"
+    >
+      <template #probe>
+        <div class="item">Probe</div>
+      </template>
+
+      <template #placeholder="{ index, style }">
+        <div
+          class="item"
+          :style="style"
+        >
+          Placeholder {{ index }}
+        </div>
+      </template>
+
+      <template #default="{ item, style }">
+        <div
+          class="item"
+          :style="style"
+        >
+          <VanCard
+            :num="item.duration"
+            :price="formatPrice(item.price)"
+            :desc="item.description"
+            :title="item.name"
+            thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg"
+          />
+        </div>
+      </template>
+    </Grid>
   </TitlebarLayout>
 </template>
 
 <script setup lang="ts">
+import Grid from 'vue-virtual-scroll-grid'
 import { useFetch } from '@/composables/useFetch'
 
 interface Course {
@@ -38,6 +64,23 @@ const { data } = useFetch<Courses>(
 const formatPrice = (price: string) => {
   return price.slice(0, -1)
 }
+
+const length = computed(() => {
+  return data.value?.courses.length ?? 0
+})
+
+const pageProvider = (): Promise<unknown[]> =>
+  new Promise((resolve) => resolve(data.value?.courses ?? []))
 </script>
 
-<style scoped></style>
+<style scoped>
+.grid {
+  display: grid;
+  grid-gap: 12px;
+  grid-template-columns: repeat(1, 1fr);
+}
+
+.item {
+  margin-top: 0;
+}
+</style>
