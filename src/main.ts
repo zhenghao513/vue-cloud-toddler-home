080e50c5a65f757bdfd20d23be6f9d89a23c3415
Zheng Hao <zhengh513@outlook.com>
2024-01-26 19:36:51 +0800

    chore: 配置 Vue I18n

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import router from './router'
 import '@/assets/js/iconfont_svg.js'
 import { createPinia } from 'pinia'
 import '@/api/interceptors'
+import i18n from './plugins/i18n'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
+app.use(i18n)
 
 app.mount('#app')
